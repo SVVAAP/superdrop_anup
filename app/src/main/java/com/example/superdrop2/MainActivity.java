15be695fb0e552a98e44ade322133a524b7113e6
SVVAAP <workwithsvvaap@gmail.com>
2023-08-25 22:28:59 +0530

    BLA BLA boom 2 fix

--- app/src/main/java/com/example/superdrop2/MainActivity.java
+++ app/src/main/java/com/example/superdrop2/MainActivity.java
@@ -43,6 +43,6 @@ public class MainActivity extends AppCompatActivity {
              //  startActivity(new Intent(MainActivity.this, NavActivity.class));
                 finish();
             }
-        },2500);
+        },2000);
     }
 }
