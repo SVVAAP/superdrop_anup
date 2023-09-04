d7a8bfb24001f2643e4cd733358acf63bbb9a552
SVVAAP <workwithsvvaap@gmail.com>
2023-09-04 19:20:35 +0530

    BLA BLA boom 2 fix

--- app/src/main/java/com/example/superdrop2/MyNotification.java
+++ app/src/main/java/com/example/superdrop2/MyNotification.java
@@ -21,33 +21,39 @@ public class MyNotification extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
         super.onMessageReceived(message);
-        Intent intent=new Intent(getApplicationContext(), OrderPlacedActivity.class);
-        if(message.getData().size()>0) {
+        Intent intent = new Intent(getApplicationContext(), Track_Order_Activity.class);
+        if (message.getData().size() > 0) {
             String title = message.getData().get("title");
-            String body=message.getData().get("body");
-            showNotification(getApplicationContext(),title,body,intent);
+            String body = message.getData().get("body");
+            showNotification(getApplicationContext(), title, body, intent);
         }
-        showNotification(getApplicationContext(),message.getNotification().getTitle(),message.getNotification().getBody(),intent);
+        showNotification(getApplicationContext(), message.getNotification().getTitle(), message.getNotification().getBody(), intent);
     }
 
-    public void showNotification(Context context,String title,String message,Intent intent){
-        NotificationManager notificationManager=(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        int notificationid=1;
-        String channelid ="Channel1";
-        String channelName="My Channel";
-        int importance=NotificationManager.IMPORTANCE_HIGH;
+    public void showNotification(Context context, String title, String message, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        int notificationId = 1;
+        String channelId = "Channel1";
+        String channelName = "My Channel";
+        int importance = NotificationManager.IMPORTANCE_HIGH;
 
-        NotificationChannel notificationChannel=new NotificationChannel(channelid,channelName,importance);
-        notificationManager.createNotificationChannel(notificationChannel);
+        // Create a notification channel (only if it doesn't exist)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, importance);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
 
-        NotificationCompat.Builder mbuilder=new NotificationCompat.Builder(context,channelid)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
                 .setSmallIcon(R.drawable.cat_2)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setAutoCancel(true);
 
-        PendingIntent intent1=PendingIntent.getActivity(context,1,intent,PendingIntent.FLAG_MUTABLE);
-        mbuilder.setContentIntent(intent1);
-        notificationManager.notify(notificationid,mbuilder.build());
+        // Create a PendingIntent to open the Track_Order_Activity
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+
+        // Notify using the NotificationManager
+        notificationManager.notify(notificationId, builder.build());
     }
 }
