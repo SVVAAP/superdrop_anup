8afc14018cd6093749603fa675f14fba10409ed2
SVVAAP <workwithsvvaap@gmail.com>
2023-09-09 21:42:39 +0530

    hiiii

--- app/src/main/java/com/example/superdrop2/adapter/ImageAdapter.java
+++ app/src/main/java/com/example/superdrop2/adapter/ImageAdapter.java
@@ -1,12 +1,10 @@
 package com.example.superdrop2.adapter;
 
 import android.content.Context;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -28,7 +26,7 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
     @NonNull
     @Override
     public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.post_view, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.offer_rv, parent, false);
         return new ImageViewHolder(v);
     }
 
