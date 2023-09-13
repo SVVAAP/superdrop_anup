b9cb47687955565cad305dd63b25baca99713fc3
SVVAAP <workwithsvvaap@gmail.com>
2023-09-13 21:12:30 +0530

    hii bu srujan

---
+++ superdrop_admin/src/main/java/com/example/superdrop_admin/OwnersTabActivity.java
@@ -0,0 +1,57 @@
+package com.example.superdrop_admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.example.superdrop_admin.adapter.TabAdapter;
+import com.google.android.material.tabs.TabLayout;
+
+public class OwnersTabActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private TabAdapter tabAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owners_tab);
+
+        tabLayout =findViewById(R.id.tab_layout);
+        viewPager2=findViewById(R.id.view_pager);
+        tabAdapter=new TabAdapter(this);
+        viewPager2.setAdapter(tabAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
+
+
+
+    }
+}
