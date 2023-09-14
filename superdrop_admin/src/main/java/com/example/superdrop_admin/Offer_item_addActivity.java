67a9bb5b26840c2c21936b70daf1770d3a68444e
SVVAAP <workwithsvvaap@gmail.com>
2023-09-14 20:31:47 +0530

    hii by srujan

---
+++ superdrop_admin/src/main/java/com/example/superdrop_admin/Offer_item_addActivity.java
@@ -0,0 +1,14 @@
+package com.example.superdrop_admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Offer_item_addActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer_item_add);
+    }
+}
