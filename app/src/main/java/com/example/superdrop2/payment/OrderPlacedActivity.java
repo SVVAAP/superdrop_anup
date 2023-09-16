980a055c7c3f961c605c83369e480eb99dcbdf30
SVVAAP <workwithsvvaap@gmail.com>
2023-09-16 19:31:33 +0530

    hii by srujan

--- app/src/main/java/com/example/superdrop2/payment/OrderPlacedActivity.java
+++ app/src/main/java/com/example/superdrop2/payment/OrderPlacedActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.superdrop2.R;
 import com.example.superdrop2.customers_Activity;
 
@@ -24,5 +26,13 @@ Button track;
                 startActivity(intent);
             }
         });
+        // Load the GIF image
+
+        ImageView gifImageView = findViewById(R.id.imageView8);
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.ic_order_placed)
+                .into(gifImageView);
+
     }
 }
