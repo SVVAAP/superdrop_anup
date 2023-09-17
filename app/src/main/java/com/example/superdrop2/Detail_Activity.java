2580010efbb6f37b748718831b550d9ca9ccc644
SVVAAP <workwithsvvaap@gmail.com>
2023-09-17 19:31:50 +0530

    hii by srujan

--- app/src/main/java/com/example/superdrop2/Detail_Activity.java
+++ app/src/main/java/com/example/superdrop2/Detail_Activity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.superdrop2.methods.User;
@@ -19,12 +21,13 @@ import com.google.firebase.messaging.FirebaseMessaging;
 
 public class Detail_Activity extends AppCompatActivity {
 
-    private EditText fullName, phoneNumber, address,city,landmark;
+    private EditText fullName, phoneNumber, address, landmark;
+    private Spinner citySpinner;
     private Button signUpButton;
     // Firebase references
     private FirebaseAuth mAuth;
     private FirebaseDatabase mDatabase;
-    private String phoneNumberget,Token;
+    private String phoneNumberget, Token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,17 +43,27 @@ public class Detail_Activity extends AppCompatActivity {
         fullName = findViewById(R.id.fullname);
         phoneNumber = findViewById(R.id.phone_number);
         address = findViewById(R.id.address_text);
-        city = findViewById(R.id.city_text);
-        landmark =findViewById(R.id.landmark_text);
+        citySpinner = findViewById(R.id.city_spinner); // Updated to use the Spinner
+        landmark = findViewById(R.id.landmark_text);
         signUpButton = findViewById(R.id.signup_button);
 
+        // Define your city options as an array of strings
+        String[] cityOptions = {"Shirva", "Belman", "Nitte"};
+
+        // Initialize the ArrayAdapter
+        ArrayAdapter<String> cityAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, cityOptions);
+
+        // Set the dropdown layout style
+        cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter to the Spinner
+        citySpinner.setAdapter(cityAdapter);
 
         FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
             if (task.isSuccessful() && task.getResult() != null) {
                 String iToken = task.getResult();
                 // Store the token in Firebase Firestore
-                Token=iToken;
-
+                Token = iToken;
             }
         });
 
@@ -69,15 +82,14 @@ public class Detail_Activity extends AppCompatActivity {
             String name = fullName.getText().toString();
             String phone_optnl = phoneNumber.getText().toString();
             String userAddress = address.getText().toString();
-            String cityAddress = city.getText().toString();
+            String selectedCity = citySpinner.getSelectedItem().toString(); // Get the selected city from the Spinner
             String landmarkAddress = landmark.getText().toString();
-
 
             // Reference to the "users" node in Firebase Database
             DatabaseReference userRef = mDatabase.getReference("users").child(userId);
 
-            // Create a User object and set the details
-            User userDetails = new User(name,phoneNumberget, phone_optnl, userAddress,cityAddress,landmarkAddress,Token);
+            // Create a User object and set the details including the selected city
+            User userDetails = new User(name, phoneNumberget, phone_optnl, userAddress, selectedCity, landmarkAddress, Token);
 
             // Push the user details to the database
             userRef.setValue(userDetails)
@@ -85,7 +97,6 @@ public class Detail_Activity extends AppCompatActivity {
                         Toast.makeText(Detail_Activity.this, "Details uploaded successfully!", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(Detail_Activity.this, NavActivity.class));
                         finish();
-
                     })
                     .addOnFailureListener(e -> {
                         Toast.makeText(Detail_Activity.this, "Failed to upload details.", Toast.LENGTH_SHORT).show();
